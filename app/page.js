79e704729f07a84fdd6258dd85e8c7d70529c13a
Rutherford16 <hidayatronny@ymail.com>
2024-09-11 14:09:29 +0800

    update

--- app/page.js
+++ app/page.js
@@ -6,93 +6,112 @@ import dataProduct from "./json/data-product.json";
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#e6d4cc]">
-      <nav className="navbar sticky-top navbar-expand-sm flex flex-row bg-[#8a5338]">
-        <img className="w-10 h-10" src="/images/logo.png" alt="Logo" />
-        <div className="input-group mx-2 rounded-xl text-white border-white border">
-          <span
-            className="input-group-text bg-transparent border-none text-white"
-            id="search-logo"
-          >
-            <i className="bi bi-search"></i>
-          </span>
-          <input
-            type="search"
-            name="cari"
-            id="cari"
-            placeholder="Cari"
-            className="bg-transparent pr-3 outline-none w-5 flex-1"
-            aria-describedby="search-logo"
-          />
-        </div>
-        <button className="w-10 h-10 mx-2">
-          <img className="w-fit h-fit" src="/images/keranjang.png" alt="" />
-        </button>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="offcanvas offcanvas-end"
-          tabIndex="-1"
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-        >
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-              Offcanvas
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
+      <nav className="navbar sticky-top navbar-expand-sm bg-[#8a5338]">
+        <div className="flex flex-row w-full px-2">
+          <img className="w-10 h-10" src="/images/logo.png" alt="Logo" />
+          <div className="input-group mx-2 rounded-xl text-white border-white border flex-1">
+            <span
+              className="input-group-text bg-transparent border-none text-white"
+              id="search-logo"
+            >
+              <i className="bi bi-search"></i>
+            </span>
+            <input
+              type="search"
+              name="cari"
+              id="cari"
+              placeholder="Cari"
+              className="bg-transparent pr-3 outline-none w-5 flex-1"
+              aria-describedby="search-logo"
+            />
           </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
+          <button className="w-10 h-10 mx-2">
+            <img className="w-fit h-fit" src="/images/keranjang.png" alt="" />
+          </button>
+          <button
+            className="text-white text-4xl outline-none"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            aria-label="Toggle navigation"
+          >
+            <i class="bi bi-list"></i>
+          </button>
+          <div
+            className="offcanvas offcanvas-end"
+            tabIndex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header bg-[#8A5338]">
+              <h5 className="offcanvas-title text-white" id="offcanvasNavbarLabel">
+                Menu
+              </h5>
+              <button
+                type="button"
+                className="btn-close-white btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <div className="flex flex-row justify-evenly">
+                    <a
+                      className="nav-link bg-[#8A5338] px-3 rounded text-white"
+                      href="#"
+                    >
+                      Daftar
                     </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
+                    <a
+                      className="nav-link bg-[#8A5338] px-3 rounded text-white"
+                      href="#"
+                    >
+                      <i class="bi bi-box-arrow-in-right mr-2"></i>
+                      Masuk
                     </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
@@ -109,7 +128,7 @@ export default function Home() {
             ></Card>
           ))}
       </div>
-      <nav className="w-full p-2 flex flex-row justify-around fixed bottom-5 rounded-full left-0 bg-[#3f2716] text-white">
+      <nav className="w-full p-2 flex flex-row justify-around fixed bottom-2 rounded-full left-0 bg-[#3f2716] text-white text-xs">
         <a href="#" className="grid justify-items-center hover:opacity-50">
           <img src="/images/Nav/buat kamu.png" className="w-8 h-auto" />
           Buat Kamu
